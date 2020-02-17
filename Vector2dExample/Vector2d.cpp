ce75f2b946f821e8d6f0f80d0a11a33415cf01e0
DmitryLaptev <60900680+DmitryLaptev@users.noreply.github.com>
2020-02-17 22:01:58 +0300

    Add files via upload

--- Vector2dExample/Vector2d.cpp
+++ Vector2dExample/Vector2d.cpp
@@ -1,6 +1,9 @@
 #include "Vector2d.h"
 #include <iostream>
 #include <cmath>
+#include <string>
+#include <cstring>
+using namespace std;
 /*Vector2d::Vector2d() 
 {
 	this->x = 0;
@@ -16,46 +19,31 @@ Vector2d::Vector2d(double x1, double y1, double x2, double y2)
 	this->x = x2 - x1;
 	(*this).y = y2 - y1;
 }
-/*void Vector2d::setx(double x)
-{
-	std::cin >> x;
-}
-double Vector2d::getx()
-{
-	return Vector2d::x;
-}
-void Vector2d::sety(double y)
-{
-	std::cin >> y;
-}
-double Vector2d::gety()
-{
-	return Vector2d::y;
-}*/
 void Vector2d::print() 
 {
 	std::cout << Vector2d::x << ' ' << Vector2d::y;
 }
-void Vector2d::sum(Vector2d b) 
+Vector2d* Vector2d::sum(Vector2d b) 
 {
 	x += b.x;
 	y += b.y;
+	return new Vector2d(x, y);
 }
-void Vector2d::sub(Vector2d b) 
+Vector2d* Vector2d::sub(Vector2d b)
 {
 	x -= b.x;
 	y -= b.y;
+	return new Vector2d(x, y);
 }
 double Vector2d::scalarMult(Vector2d b) 
 {
-	x *= b.x;
-	y *= b.y;
-	return x + y;
+	return x*b.x + y*b.y;
 }
-void Vector2d::mult(double value) 
+Vector2d* Vector2d::mult(double value) 
 {
 	x *= value;
 	y *= value;
+	return new Vector2d(x, y);
 }
 double Vector2d::length()
 {
@@ -63,30 +51,128 @@ double Vector2d::length()
 }
 double Vector2d::tanAngle(Vector2d b)
 {
-	x *= b.y;
-	y *= b.x;
-	return (x - y) / scalarMult(b);
+	return (x*b.y - y*b.x) / scalarMult(b);
+}
+Vector2d Vector2d::operator+(const Vector2d& v) 
+{
+	return Vector2d(x + v.x, y + v.y);
+}
+Vector2d Vector2d::operator-(const Vector2d& v) 
+{
+	return Vector2d(x - v.x, y - v.y);
+}
+double Vector2d::operator/(Vector2d v) 
+{
+	return x*v.x + y*v.y;
+}
+Vector2d operator*(Vector2d& vector, double realNumber)
+{
+	return Vector2d(realNumber * vector.x, realNumber * vector.y);
+}
+Vector2d& Vector2d::operator++() 
+{
+	x++;
+	y++;
+	return *this;
+}
+Vector2d Vector2d::operator++(int) 
+{
+	Vector2d temp(x, y);
+	x++;
+	y++;
+	return temp;
+}
+Vector2d& Vector2d::operator--()
+{
+	x--;
+	y--;
+	return *this;
+}
+Vector2d Vector2d::operator--(int)
+{
+	Vector2d temp(x, y);
+	x--;
+	y--;
+	return temp;
+}
+const Vector2d& Vector2d::operator+=(const Vector2d& vector)
+{
+	x += vector.x;
+	y += vector.y;
+	return *this;
+}
+const Vector2d& Vector2d::operator-=(const Vector2d& vector)
+{
+	x -= vector.x;
+	y -= vector.y;
+	return *this;
+}
+const Vector2d& Vector2d::operator*=(const Vector2d& vector)
+{
+	x *= vector.x;
+	y *= vector.y;
+	return *this;
+}
+Vector2d::operator string() 
+{
+	string s = "(" + to_string(x) + ";" + to_string(y) + ")";
+	return s;
 }
 void main() 
 {
+	setlocale(LC_ALL, "Russian");
 	Vector2d a(3, 5);
+	cout << "Вектор a:" << endl;
+	cout << "Введите координату x:";
+	a.setx();
+	cout << "Введите координату y:";
+	a.sety();
 	Vector2d b(4, 8);
-	a.sum(b);
-	a.print();
-	std::cout << std::endl;
-	a.sub(b);
-	a.print();
-	std::cout << std::endl;
-	a.scalarMult(b);
-	a.print();
-	std::cout << std::endl;
-	a.mult(4);
-	a.print();
-	std::cout << std::endl;
-	a.length();
-	a.print();
-	std::cout << std::endl;
-	a.tanAngle(b);
-	a.print();
-	std::cout << std::endl;
+	cout << "Вектор b:" << endl;
+	cout << "Введите координату x:";
+	b.setx();
+	cout << "Введите координату y:";
+	b.sety();
+	Vector2d c = a + b;
+	cout << "Сложение векторов:";
+	string s = c.operator std::string();
+	cout << s << endl;
+	c = a - b;
+	cout << "Разность векторов:";
+	s = c.operator std::string();
+	cout << s << endl;
+	double scal = a / b;
+	cout << "Скалярное произведение векторов:";
+	cout << scal << endl;
+	cout << "Умножение вектора b на число" << endl;
+	double ch;
+	cout << "Введите число:";
+	cin >> ch;
+	c = b * ch;
+	cout << "Результат умножения:";
+	s = c.operator std::string();
+	cout << s << endl;
+	double tangens=a.tanAngle(b);
+	cout << "Тангенс угла между векторами:";
+	cout << tangens << endl;
+	cout << "Инкремент";
+	c++;
+	s = c.operator std::string();
+	cout << s << endl;
+	cout << "Декремент";
+	c--;
+	s = c.operator std::string();
+	cout << s << endl;
+	c += b;
+	cout << "Увеличение на координаты вектора b:";
+	s = c.operator std::string();
+	cout << s << endl;
+	c -= a;
+	cout << "Уменьшение на координаты вектора a:";
+	s = c.operator std::string();
+	cout << s << endl;
+	cout << "Умножение на вектор b:" << endl;
+	c *= b;
+	s = c.operator std::string();
+	cout << s << endl;
 }
--- Vector2dExample/Vector2d.h
+++ Vector2dExample/Vector2d.h
@@ -1,29 +1,51 @@
 #pragma once
+using namespace std;
+#include <string>
+#include <iostream>
 class Vector2d
 {
 private:
 	double x, y;
 public:
+	//методы
 	Vector2d() { x = 0; y = 0; }
 	Vector2d(double, double);
 	Vector2d(double, double, double, double);
 	//Vector2d(const Vector2d&);
-	/*Vector2d(const Vector2d& vector2d) :
+	Vector2d(const Vector2d& vector2d) :
 		x(vector2d.x), y(vector2d.y)
 	{
 
-	}*/
-	//~Vector2d();
-	void setx(double value) { x = value; };
+	}
+	~Vector2d() 
+	{
+		cout << "Enabled" << endl;
+	}
+	void setx() { cin >> x; };
 	double getx() { return x; }
-	void sety(double value) { y = value; }
+	void sety() { cin >> y; }
 	double gety() { return y; }
 	void print(void);
-	void sum(Vector2d);
-	void sub(Vector2d);
+
+	Vector2d* sum(Vector2d);
+	Vector2d* sub(Vector2d);
 	double scalarMult(Vector2d);
-	void mult(double);
+	Vector2d* mult(double);
 	double length();
 	double tanAngle(Vector2d);
+	
+	//перегрузка
+	Vector2d operator+(const Vector2d& v);
+	Vector2d operator-(const Vector2d& v);
+	double operator/(Vector2d v);
+	friend Vector2d operator*(Vector2d&, double);
+	Vector2d& operator++();
+	Vector2d operator++(int);
+	Vector2d& operator--();
+	Vector2d operator--(int);
+	const Vector2d& operator+=(const Vector2d&);
+	const Vector2d& operator-=(const Vector2d&);
+	const Vector2d& operator*=(const Vector2d&);
+	operator string();
 };
 
